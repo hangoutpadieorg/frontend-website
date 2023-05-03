4490abce2b42c41d074679f54836a6fd78a80acc
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-05-03 04:23:31 +0100

    Image links fix

--- src/components/guide.js
+++ src/components/guide.js
@@ -2,28 +2,28 @@
 export default [
   {
     id: 1,
-    icon: '../src/assets/location-icon-orange.svg',
+    icon: './src/assets/location-icon-orange.svg',
     title: 'Select location',
     description: 'Choose the location where you would like to hangout.'
   },
 
   {
     id: 2,
-    icon: '../src/assets/Menu.svg',
+    icon: './src/assets/Menu.svg',
     title: 'Choose a Hangout Spot',
     description: 'Check over hundreds of hangout locations to pick your desired spot'
   },
 
   {
     id: 3,
-    icon: '../src/assets/invoice.svg',
+    icon: './src/assets/invoice.svg',
     title: 'Book in advance',
     description: "It's quick, safe, and simple. Book your space in advanced"
   },
 
   {
     id: 4,
-    icon: '../src/assets/Donut.svg',
+    icon: './src/assets/Donut.svg',
     title: 'Enjoy every moment',
     description: 'Relax, connect and enjoy every moment.'
   },
